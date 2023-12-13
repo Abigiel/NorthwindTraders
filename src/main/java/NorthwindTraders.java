88b17bbe4976cc4786bc8d413cc6132e893ea0fd
Abigiel <abigailtwg@gmail.com>
2023-12-13 10:24:04 -0500

    changed to datasource

--- src/main/java/NorthwindTraders.java
+++ src/main/java/NorthwindTraders.java
@@ -1,23 +1,50 @@
 import java.sql.*;
-import javax.sql.DataSource;
+import java.util.Scanner;
+
 
 public class NorthwindTraders {
+    static String url = "jdbc:mysql://localhost:3306/northwind";
+    static String user = "root";
+    static String password = "Harrypotter_15";
+    static ResultSet rs = null;
+    //        Statement stmt = null;
+
+    static Connection conn = null;
     public static void main(String[] args) throws ClassNotFoundException {
+
         Class.forName("com.mysql.cj.jdbc.Driver");
 
-
-        String url = "jdbc:mysql://localhost:3306/northwind";
-        String user = "username";
-        String password = "password";
-
-        String query = "SELECT * FROM Products";
+        System.out.println("What do you want to do?\n" +
+                " 1) Display all products\n" +
+                " 2) Display all customers\n" +
+                " 0) Exit\n" +
+                "Select an option: ");
+        Scanner scan = new Scanner(System.in);
+        String choice = scan.nextLine();
+        switch (choice) {
+            case "1":
+                products();
+                break;
+            case "2":
+                customers();
+                break;
+            case "0":
+                System.exit(0);
+            default:
+                System.out.println("Invalid option.");
+        }
+    }
+    public static void products() {
+        PreparedStatement preparedStatement = null;
         try {
             // Establishing connection
-            Connection conn = DriverManager.getConnection(url, user, password);
-            Statement stmt = conn.createStatement();
+            conn = DriverManager.getConnection(url, user, password);
+            //stmt = conn.createStatement();
+
+            preparedStatement = conn.prepareStatement("SELECT * FROM Products");
 
             // Executing query
-            ResultSet rs = stmt.executeQuery(query);
+            rs = preparedStatement.executeQuery();
 
             // Processing the result set
             while (rs.next()) {
@@ -30,12 +57,84 @@ public class NorthwindTraders {
             }
 
             // Closing resources
-            rs.close();
-            stmt.close();
-            conn.close();
+
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            // close the resources
+            if (rs == null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+    }
+    public static void customers(){
+        PreparedStatement preparedStatement = null;
+        try {
+            // Establishing connection
+            conn = DriverManager.getConnection(url, user, password);
+            //stmt = conn.createStatement();
 
+            preparedStatement = conn.prepareStatement("SELECT * FROM Customers ORDER BY Country");
+//                    ContactName, CompanyName, City, Country, Phone
+            // Executing query
+            rs = preparedStatement.executeQuery();
+
+            // Processing the result set
+            while (rs.next()) {
+                System.out.println("\nContact Name: " + rs.getString("ContactName"));
+                System.out.println("Company Name: " + rs.getString("CompanyName"));
+                System.out.println("City: " + rs.getString("City"));
+                System.out.println("Country: " + rs.getString("Country"));
+                System.out.println("Phone: " + rs.getString("Phone"));
+                System.out.println("-------------------------------------");
+
+            }
+
+            // Closing resources
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // close the resources
+            if (rs == null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
